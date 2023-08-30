d0bf1fa36885e92d797e484dd8a869244fb4d252
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-30 19:38:28 +0500

    size of bool

---
+++ sizeOfDataTypes.cpp
@@ -0,0 +1,10 @@
+//Size of bool, char, short int, int,  float, wchar_t, long int, signed long int, unsigned long int, double//
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Here is the size of following data types."<<endl;
+	
+	cout<<"\nSize of bool is "<<sizeof(bool)<<" Byte."<<endl;
+	return 0;
+}
