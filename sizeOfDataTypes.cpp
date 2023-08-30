8fcdd41f48fda97a7d225b6193e00e2746d8d653
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-30 19:57:33 +0500

    Size of data tpes

--- sizeOfDataTypes.cpp
+++ sizeOfDataTypes.cpp
@@ -6,5 +6,15 @@ int main()
 	cout<<"Here is the size of following data types."<<endl;
 	
 	cout<<"\nSize of bool is "<<sizeof(bool)<<" Byte."<<endl;
+	cout<<"\ize of char is "<<sizeof(char)<<" Byte."<<endl;
+	cout<<"\Size of short int is "<<sizeof(short int)<<" Byte."<<endl;
+	cout<<"\ize of wchar_t is "<<sizeof(wchar_t)<<" Byte."<<endl;
+	cout<<"\ize of int is "<<sizeof(int)<<" Byte."<<endl;
+	cout<<"\Size of float is "<<sizeof(float)<<" Byte."<<endl;
+	cout<<"\Size of long int is "<<sizeof(long int)<<" Byte."<<endl;
+	cout<<"\Size of signed long int is "<<sizeof(signed long int)<<" Byte."<<endl;
+	cout<<"\Size of unsigned long int is "<<sizeof(unsigned long int)<<" Byte."<<endl;
+	cout<<"\Size of double is "<<sizeof(double)<<" Byte."<<endl;
+	
 	return 0;
 }
